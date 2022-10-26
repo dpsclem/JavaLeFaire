2904fc3a753f19c09db4439a0e24f345ef0d5629
Clément <cledupas@gmail.com>
2022-10-26 21:56:19 +0200

    .

---
+++ src/main/java/com/example/javalefaire/JavaLeFaireApplication.java
@@ -0,0 +1,13 @@
+package com.example.javalefaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLeFaireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLeFaireApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/javalefaire/controller/MatiereController.java
@@ -0,0 +1,5 @@
+package com.example.javalefaire.controller;
+
+public class MatiereController {
+    
+}
