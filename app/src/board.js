3dc54628bafbc7348357e38e179354682e241014
Julien Condamine <julien.condamine@gmail.com>
2022-11-01 11:45:37 +0100

    character created

--- app/src/board.js
+++ app/src/board.js
@@ -1,18 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import {images} from "./entities"
 
 
 class BoardComponent extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            'characterX' : 0,
+            'characterY' : 0
+        };
+    }
     render(){
-        const cells = [];
-        for (let i = 0; i < 10; i++) {
-            cells.push(<td className="cell" key={i.toString()}></td>);
-        }
         const rows = [];
         for (let i = 0; i < 10; i++) {
-            rows.push(<tr key={i.toString()}>{cells}</tr>);
+            const cells = [];
+            for (let j = 0; j < 10; j++) {
+                if((j === this.state.characterX) && (i === this.state.characterY)){
+                    cells.push(<td className="cell" key={i.toString() + ":" + j.toString()}><img className='entities' src={images.character} /></td>);
+                }
+                else{
+                    cells.push(<td className="cell" key={i.toString() + ":" + j.toString()}></td>);
+                }
+            }
+            rows.push(<tr>{cells}</tr>);
         }
+
         return (
             <table>
                 {rows}
---
+++ app/src/entities.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import BoardComponent from "./board";
+
+export const images = {
+    'character' : './character.png'
+};
